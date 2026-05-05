ac81cb1b0229a37f9cb64ce8293c3618ee183264
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2026-05-06 10:18:46 +1200

    update elsevier

--- tools/get_elsevier_journals.py
+++ tools/get_elsevier_journals.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python3
 # coding=utf-8
 """..."""
-
-import requests
-from io import BytesIO
+import csv
+import sys
 
 from openpyxl import load_workbook
+
 
 # from https://www.elsevier.com/open-access
 
 def read(filename):
     wb = load_workbook(filename = filename)
-    header = ['ISSN', 'Title', 'Business model', 'USD', 'EUR', 'GBP', 'JPY']
+    header = None
     for i, row in enumerate(wb.worksheets[0].iter_rows()):
-        if i > 3 and row[1].value is not None:
+        if row[0].value == 'ISSN':
+            header = [r.value for r in row]
+        elif header:
             d = dict(zip(header, [c.value for c in row]))
-            d['URL'] = row[1].hyperlink.target
             yield d
 
 
-def format_journal(j):
-    if ',' in j:
-        return '"%s"' % j
-    else:
-        return j
+writer = csv.writer(sys.stdout)
+writer.writerow(['Date', 'Journal', 'Publisher', 'Cost', 'URL', 'Comment'])
 
-
-
-for o in read('elsevier-2025-03-28.xlsx'):
-    # Date,Journal,Publisher,Cost,URL,Comment
+for o in read('elsevier-2026-05-06.xlsx'):
     if o['USD'] == '**':
-        # APC is waived (new journal), or sponsored by a third party, or invoiced directly to authors. 
+        # APC is waived (new journal), or sponsored by a third party, or invoiced directly to authors.
         # ignore these for now
         continue
-    print("2025-03-28,%s,Elsevier,%s,%s,%s" % (
-        format_journal(o['Title']),
-        "$%d" % o['USD'],
-        o['URL'],
-        o['Business model']
-    ))
 
+    if o['USD'] is not None:  # could be 0
+        amt = "$%d" % o['USD']
+    elif o['EUR'] is not None:
+        amt = "€%d" % o['EUR']
+    elif o['GBP'] is not None:
+        amt = "£%d" % o['GBP']
+    else:
+        raise ValueError("No price - use JPY?")  # don't want to add another currency if I don't have to
+
+    writer.writerow([
+        '2026-05-06',
+        o['Title'],
+        'Elsevier',
+        amt,
+        f"https://www.elsevier.com/locate/issn/{o['ISSN']}",
+        o['Business model'],
+    ])
